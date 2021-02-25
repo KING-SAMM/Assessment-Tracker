d905e677661c796d031dcd701902c21327233a7a
kcsamm <kcsamm11@gmail.com>
2021-02-25 18:54:40 +0100

    Implemented DB connection, fetching data from DB Updated

--- config/DB.js
+++ config/DB.js
@@ -5,9 +5,7 @@ mongoose.Promise = global.Promise;
 
 // Mongoose Connect
 mongoose.connect(
-    /* 'mongodb+srv://Owner:%26%23oMnEr4581@cluster0.9lwab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-     'mongodb+srv://KCSamm:kcEpIsTeMoLoGy4581@cluster0.z83z1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', */
-    'mongodb+srv://KCSamm:kcEpIsTeMoLoGy4581@score.snwx3.mongodb.net/assessmentScore?retryWrites=true&w=majority', 
+    // Connection string here, 
     { 
         useNewUrlParser: true, 
         useUnifiedTopology: true,
@@ -18,6 +16,3 @@ mongoose.connect(
 .then(() => console.log('MongoDB Connected!'))
 .catch(err => console.log(err));
 
-// kcEpIsTeMoLoGy4581
-
-// Current IP: 102.89.0.234
